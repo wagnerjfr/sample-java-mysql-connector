ac488cf449d385203af02cdad83fe6b126ba5d1a
Wagner Franchin <wagner.franchin@oracle.com>
2020-07-05 09:14:16 +0100

    mysql connector/j updated

--- src/main/java/mysqlserver/Utils.java
+++ src/main/java/mysqlserver/Utils.java
@@ -3,7 +3,7 @@ package mysqlserver;
 public class Utils {
 
     // Change below path to point to your MySQL binaries
-    public static final String MYSQL_PATH = "/home/wfranchi/MySQL/mysql-8.0.11";
+    public static final String MYSQL_PATH = "/path/to/MySQL/mysql-8.0.20";
 
     public static final String CONNECT_PROPS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&verifyServerCertificate=false&useSSL=true";
 
